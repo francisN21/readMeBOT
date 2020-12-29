b242752fc99262c0ee86f1f3409f7dc18b04b624
Francisco Rones <ininorones@gmail.com>
2020-12-28 20:03:37 -0800

    Update: utils updated to ES6 syntax

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,8 @@
 // function to generate markdown for README
-function generateMarkdown(uOutput) {
+const generateMarkdown = uOutput => {
   return `# ${uOutput.title}
 
 `;
-}
+};
 
 module.exports = generateMarkdown;
