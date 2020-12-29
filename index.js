ca184d0f0d8208e80bcf84f0e5b20d41423d2489
Francisco Rones <ininorones@gmail.com>
2020-12-28 17:57:48 -0800

    Added: New start function and converted functions to arrow functions: ES6 Syntax

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
+// required const for the 
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { createConnection } = require('net');
+const util = require('util');
+
+
+
+
 // array of questions for user
 const questions = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+const writeToFile = (fileName, data) => {
+};
 
 // function to initialize program
-function init() {
+const init = () => {
 
+};
+
+
+
+const start = () => {
+  inquirer.prompt({
+    name: "StartorExit",
+    type: "list",
+    message: "This app enables you to Create a README.md file. Would you like to proceed?",
+    choices: ["Start", "Exit"]
+  })
+  .then(answer => {
+      if(answer.StartorExit === "Start"){
+          // function call to initialize program
+          init();
+      } else {
+          connection.end();
+          process.exit(0);
+      }
+  })
 }
 
-// function call to initialize program
-init();
+start();
